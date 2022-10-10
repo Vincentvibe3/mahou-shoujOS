313453a04ec872475d558128c4f680cfdfe26197
Vincent <vincentvibe3@gmail.com>
2022-10-10 19:53:19 -0400

    use web-greeter

--- install.sh
+++ install.sh
@@ -48,6 +48,7 @@ cp -r ./config/* ~/.config
 sudo systemctl enable lightdm.service
 sudo mkdir -p /usr/share/backgrounds/lightdm
 sudo sed -i 's/#greeter-session=.*\n/greeter-session=web-greeter\n/g' /etc/lightdm/lightdm.conf
+sudo sed -i 's/greeter-session=.*\n/greeter-session=web-greeter\n/g' /etc/lightdm/lightdm.conf
 sudo cp ./lightdm/lightdm-webkit2-greeter.conf /etc/lightdm/lightdm-webkit2-greeter.conf
 sudo cp ./lightdm/madoka.png /usr/share/backgrounds/lightdm/madoka.png
 
