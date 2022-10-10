76f39a8b700ec5c9711624610afd7e2628fb3863
Vincent <vincentvibe3@gmail.com>
2022-10-10 17:16:37 -0400

    remove os-release-edit.sh

--- os-release-edit.sh
+++
@@ -1,3 +0,0 @@
-sudo sed -i 's/NAME="Arch Linux"/NAME="Mahou ShoujOS"/g' /etc/os-release
-sudo sed -i 's/PRETTY_NAME="Arch Linux"/PRETTY_NAME="Mahou ShoujOS"/g' /etc/os-release
-sudo sed -i 's/ID=arch/ID=mahoushoujos/g' /etc/os-release
